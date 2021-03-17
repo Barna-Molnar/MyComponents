05697be1d6adf616120dad66a3486967872b631d
Barnabas Molnar <hanta911@gmail.com>
2021-03-17 20:00:38 +0100

    Creating Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import "./App.scss";
-import LoginPage from "./LoginPageComp/LoginPage.jsx";
-import NewTabs from "./NewTabs/NewTabs.jsx";
-import Cards from "./Cards/Cards.jsx";
+import './App.scss';
+import LoginPage from './LoginPageComp/LoginPage.jsx';
+import NewTabs from './NewTabs/NewTabs.jsx';
+// import Cards from './Cards/Cards.jsx';
+import Button from './Buttons/Button.jsx';
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginPage /> */}
-      <Cards />
-      {/* <NewTabs /> */}
+      <Button />
     </div>
   );
 }
