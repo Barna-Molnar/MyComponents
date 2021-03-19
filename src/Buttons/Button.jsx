99e6760ec20b4e009fcad292d1808d39020943ea
Barnabas Molnar <hanta911@gmail.com>
2021-03-19 19:10:30 +0100

    Playing with background and hover

--- src/Buttons/Button.jsx
+++ src/Buttons/Button.jsx
@@ -9,6 +9,8 @@ const Button = () => {
       <a href="#5" className="link-1">
         Link Hover
       </a>
+      <div className="test-1">Link Hover2</div>
+      <div className="test-2">Link Hover3</div>
     </div>
   );
 };
